1575e6d87fef246c9e90861b3147cb906b287917
MAVZEn <zozz908y5@gmail.com>
2026-09-24 03:17:57 +0300

    -build profile screen

---
+++ lib/view/screens/profile_screen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  var fullName = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF5F7FB),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            SizedBox(height: 100),
+            Container(
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: Color(0xffE8ECF5),
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Icon(Icons.person, size: 50, color: Color(0xff3f51b5)),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Create Your Profile",
+              style: TextStyle(fontSize: 20, fontWeight: .bold),
+            ),
+            SizedBox(height: 20),
+
+            CustomTextFormField(
+              label: "Full Name",
+              controller: fullName,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Enter your name";
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20),
+            MaterialButton(
+              onPressed: () {},
+              color: Color(0xff3f51b5),
+              padding: EdgeInsets.all(10),
+              minWidth: 300,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Text(
+                "Create",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: .bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    this.controller,
+    this.validator,
+    required this.label,
+  });
+  final TextEditingController? controller;
+  final String? Function(String?)? validator;
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: .start,
+      mainAxisSize: .min,
+      children: [
+        Text(label, style: TextStyle(fontSize: 16, fontWeight: .bold)),
+        SizedBox(height: 15),
+        TextFormField(
+          controller: controller,
+          validator: validator,
+          decoration: InputDecoration(
+            hint: Text(
+              "Enter your name",
+              style: TextStyle(color: CupertinoColors.inactiveGray),
+            ),
+            fillColor: Colors.white,
+            filled: true,
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.transparent),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.redAccent),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.redAccent),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
